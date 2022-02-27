249b5febde232dfde5dd7d5b7fabfb8c2c1a9696
rajveer <rallrajveer@gmail.com>
2022-02-27 18:02:39 +0530

    finished

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -7,9 +7,7 @@ class QuizBrain {
   var _questionNumber = 0;
 
   List _questionBank = [
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
+
     Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
